17d2325c48329c948c85092155fb6d70821e44bb
Rodolfo Berrone <rjberrone@globalsoft.dev>
2023-10-09 19:15:24 +0000

    cargue las tablas en raw

---
+++ models/CAPA-RAW/schema.sql
@@ -0,0 +1,18 @@
+
+version: 2
+
+sources:
+  - name: g_drive_javier_ecommerce
+    schema: g_drive_javier_ecommerce
+    tables:
+      - name: stores
+      - name: customers
+      - name: geolocation
+      - name: order_items
+      - name: order_payments
+      - name: order_reviews
+      - name: orders
+      - name: product_category
+      - name: products
+      - name: sellers
+      
