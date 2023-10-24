4fd26ddf0c163ae53baff90beeddf096afc72c7f
Rodolfo Berrone <rjberrone@globalsoft.dev>
2023-10-24 01:28:54 +0000

    correcciones

--- models/CAPA-TRUSTED/customers.sql
+++ models/CAPA-TRUSTED/customers.sql
@@ -7,4 +7,3 @@ select
     trim(upper(customer_city)) as customer_city,
     trim(upper(customer_state)) as customer_state
 from {{ source("ecommerce_raw", "customers") }}
-where customer_id is not null and customer_unique_id is not null
