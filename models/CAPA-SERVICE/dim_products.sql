8637770abd8b2ef48743d8b751b1ad8db5381dc4
Rodolfo Berrone <rjberrone@globalsoft.dev>
2023-11-13 23:36:50 +0000

    cambios menores

--- models/CAPA-SERVICE/dim_products.sql
+++ models/CAPA-SERVICE/dim_products.sql
@@ -7,5 +7,5 @@ SELECT
     , p.PRODUCT_WEIGHT_G as PRODUCT_WEIGHT
     , p.PRODUCT_LENGTH_CM * p.PRODUCT_HEIGHT_CM * p.PRODUCT_WIDTH_CM as PRODUCT_VOLUME
 FROM {{ ref('products') }} p
-    FULL JOIN {{ ref('order_items') }} o_i
+    JOIN {{ ref('order_items') }} o_i
         ON p.product_id = o_i.order_id
